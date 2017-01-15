b1c535936b330fcb734841e1697baa752a5c1714
Jomm Naja <jomgang@hotmail.com>
2017-01-16 04:36:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -66,7 +66,7 @@ if (!is_null($events['events'])) {
 				];				
 				$template = [
 					'type' => '$type',
-					'columns' => [$columns],
+					'columns' => $columns,
 				];				
 				$messages = [
 					'type' => 'template',
