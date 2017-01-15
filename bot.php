84c43fc40a36e9b8fbd848345317400a8ad98897
Jomm Naja <jomgang@hotmail.com>
2017-01-16 03:07:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -110,14 +110,15 @@ if (!is_null($events['events'])) {
 					'address' => 'ศูนย์การประชุมแห่งชาติสิริกิติ์',
 					'latitude' => 13.723702,
 					'longitude' => 100.559159,
-						],[
+						],
+					[
 					'type' => 'text',
 					'text' => $replytext,
-						]
+						],
 				];
 				$data = [
 					'replyToken' => $replyToken,
-					'messages' => [$messages],
+					'messages' => $messages,
 				];
 				
 			}else{
