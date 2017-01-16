73f36981cd76fc9698ec417b29b4cf929ed04fd6
Jomm Naja <jomgang@hotmail.com>
2017-01-16 13:39:48 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,40 @@
+<?php
+    $actions = [
+        [
+        'type' => 'postback',
+        'label' => 'จัดที่ไหน',
+        'text' => 'จัดที่ไหน',
+        'data' => 'message=ไหน',
+            ],[
+        'type' => 'postback',
+        'label' => 'แผนผังงาน',
+        'text' => 'แผนผังงาน',
+        'data' => 'message=ไหร่',
+            ],[
+        'type' => 'postback',
+        'label' => 'กิจกรรมวันนี้',
+        'text' => 'กิจกรรมวันนี้',
+        'data' => 'message=ไหร่',
+            ],[
+        'type' => 'uri',
+        'label' => 'TITF Facebook',
+        'data' => 'message=ไหร่',
+        'uri' => 'https://www.facebook.com/ttaatitf',
+            ]
+    ];				
+    $template = [
+        'type' => 'buttons',
+        'text' => 'เลือกเมนู หรือพิมพ์ keyword เพื่อค้นหาบูธในงานที่ต้องการ',
+        'actions' => $actions,
+    ];				
+    $messages = [
+        'type' => $type,
+        'altText' => 'โปรดเลือกปุ่ม',
+        'template' => $template,
+    ];
+    $data = [
+        'replyToken' => $replyToken,
+        'messages' => [$messages],
+    ];
+
+    echo json_encode($data);
